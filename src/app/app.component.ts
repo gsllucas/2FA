d8316c94f0ffbbe28bc71fb24215dbf8f1c1998b
gsllucas <lucas.goncalves@coopersystem.com.br>
2022-07-31 15:47:21 -0300

    :boom: Twitch code security clone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChildren('fields')
+  fieldsRef?: QueryList<ElementRef>;
+
   title = 'fc';
+
+  codeFields: CodeFields[] = [
+    { name: 'fieldOne', value: '' },
+    { name: 'fieldTwo', value: '' },
+    { name: 'fieldThree', value: '' },
+    { name: 'fieldFour', value: '' },
+    { name: 'fieldFive', value: '' },
+  ];
+
+  disabled = true;
+  loading = false;
+
+  constructor(private toastr: ToastrService) {}
+
+  @HostListener('window:keypress', ['$event'])
+  submitVote(event: KeyboardEvent) {
+    if (!this.disabled && event.key === 'Enter') this.sendVote();
+  }
+
+  ngOnInit(): void {}
+
+  handleEvent({ target }: any, index: number): void {
+    const { value } = target;
+
+    if (index < this.codeFields.length - 1 && value !== '')
+      this.focusNextField(index);
+
+    if (value === '' && index > 0) this.focusLastField(index);
+
+    this.codeFields[index].value = value;
+    this.setButtonState();
+  }
+
+  focusNextField(index: number) {
+    this.fieldsRef!.toArray()[index + 1].nativeElement.focus();
+  }
+
+  focusLastField(index: number) {
+    this.fieldsRef!.toArray()[index - 1].nativeElement.focus();
+  }
+
+  sendVote() {
+    const rightCode = 87463;
+    const code = Number(
+      this.codeFields.reduce((acc, field) => (acc += field.value), '').trim()
+    );
+    this.loading = true;
+    this.disabled = true;
+    setTimeout(() => {
+      if (code === rightCode) this.toastr.success('Sucesso!');
+      else this.toastr.error('O código não é válido.');
+      this.loading = false;
+      this.disabled = false;
+      this.clearAll();
+    }, 2000);
+  }
+
+  splitCode(event: ClipboardEvent) {
+    const pasted = event.clipboardData!.getData('Text');
+    const codeSplitted = pasted?.trim().split('');
+    codeSplitted.map(
+      (_, index) => (this.codeFields[index].value = codeSplitted[index])
+    );
+    this.fieldsRef?.toArray()[codeSplitted.length - 1].nativeElement.focus();
+    this.setButtonState();
+  }
+
+  setButtonState() {
+    this.disabled = this.codeFields.some(
+      (field) => field.value === '' || field.value === null
+    );
+  }
+
+  clear(index: number) {
+    this.codeFields[index].value = '';
+    this.setButtonState();
+  }
+
+  clearAll() {
+    this.codeFields.map((field) => (field.value = ''));
+  }
+}
+
+interface CodeFields {
+  name: string;
+  value?: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
+import { NgxMaskModule } from 'ngx-mask';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxMaskModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      closeButton: true,
+      preventDuplicates: true,
+      progressBar: false,
+      progressAnimation: 'increasing',
+      positionClass: 'toast-top-right',
+    }),
+    NoopAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
